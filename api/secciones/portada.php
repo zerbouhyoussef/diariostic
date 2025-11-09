a76cb18fd4dde75f9cc5157d71c03507991a0036
youssef <zerbouhyoussef@hotmail.com>
2025-11-09 17:06:05 +0100

    update title

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada",
+"titulo" => "Portada actualizada",
 "autor" => "mtorres",
 "resumen" => "El resumen maravilloso",
 ];
